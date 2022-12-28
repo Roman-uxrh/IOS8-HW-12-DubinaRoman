75d0eddf04b4fa0eeb9a970aaecd46368fa44e2d
RRDubina <dubinaroman.dubina@gmail.com>
2022-12-28 23:15:28 +0500

    Change - add button

--- IOS8-HW-12-DubinaRoman/Sourse/ViewController.swift
+++ IOS8-HW-12-DubinaRoman/Sourse/ViewController.swift
@@ -16,9 +16,18 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.text = "1"
         label.textColor = .black
-        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.font = UIFont.boldSystemFont(ofSize: 80)
         label.textAlignment = .center
         return label
+    }()
+    
+    private lazy var startButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 25
+        button.backgroundColor = .black
+        return button
     }()
 
     // MARK: - Life Cycle
@@ -38,12 +47,21 @@ class ViewController: UIViewController {
     
     private func setupHierarchy() {
         view.addSubview(labelTimer)
+        view.addSubview(startButton)
     }
     
     private func setupLayout() {
+        
         labelTimer.snp.makeConstraints { make in
+            make.centerY.equalTo(view).offset(-170)
             make.centerX.equalTo(view)
-            make.centerY.equalTo(view)
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.top.equalTo(labelTimer.snp.bottom).offset(60)
+            make.centerX.equalTo(view)
+            make.height.equalTo(50)
+            make.width.equalTo(50)
         }
     }
     
